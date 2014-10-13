48d1c6b2377b1bbc1311b62d018126b012198ffa
ruthearle <rrearle@gmail.com>
2014-10-13 15:58:00 +0100

    test pass for getting a Json response for user information

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,6 +1,10 @@
 class ApiController < ApplicationController
 
 	def getuser
-	end
-
+    @users = User.all
+    @users.each do |user|
+      render json: { first_name: user.name_first, last_name: user.name_last, job_title: user.job_title,
+                     location: user.location, bio: user.bio, latitude: user.latitude, longitude: user.longitude }
+	    end
+    end
 end
