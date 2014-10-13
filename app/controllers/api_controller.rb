133a1c6d3ce3b70d5f17cc0c46da2d19fad35d06
ruthearle <rrearle@gmail.com>
2014-10-13 17:28:26 +0100

    json now rendering for user information

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,8 @@
 class ApiController < ApplicationController
 
-	def getuser
+  def getuser
     @users = User.all
-    @users.each do |user|
-      render json: { first_name: user.name_first, last_name: user.name_last, job_title: user.job_title,
-                     location: user.location, bio: user.bio, latitude: user.latitude, longitude: user.longitude }
-	    end
-    end
+    render json: @users.sort{|a,b| a.created_at <=> b.created_at}
+  end
+
 end
