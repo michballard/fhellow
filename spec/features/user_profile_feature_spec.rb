45236485beee119c95f4391fc97fe8fc0a9f480f
michballard <michballard@gmail.com>
2014-10-14 18:21:06 +0100

    Test added for adding a profile image

--- spec/features/user_profile_feature_spec.rb
+++ spec/features/user_profile_feature_spec.rb
@@ -43,5 +43,14 @@ describe 'user profiles' do
       expect(page).to have_content('www.barnany.com') 
       expect(page).to have_content('linkedin.com/in/barnany') 
     end 
+
+    it 'can add a profile image' do 
+      user_sign_up
+      add_basic_details
+      attach_file 'Avatar', ('spec/fixtures/images/avatar.jpg')
+      click_button('Create my profile')
+      expect(page).to have_css('.avatar')
+      expect(@user.avatar_file_name).to eq "avatar.jpg"
+    end 
   end 
 end
