f0ae6d50987903c905db0e527243d8c2e4eb9fb4
michballard <michballard@gmail.com>
2014-10-14 17:43:03 +0100

    Test added for adding location to profile

--- spec/features/user_profile_feature_spec.rb
+++ spec/features/user_profile_feature_spec.rb
@@ -12,28 +12,25 @@ describe 'user profiles' do
       expect(current_path).to eq "/users/4/edit_profile"
 	  end 
 
-    it 'can edit profile details' do
+    it 'can edit basic profile details' do
       user_sign_up
-      puts page.html
       expect(page).to have_content('First name')
-      fill_in('user[name_first]', with: 'Barnany')
-      fill_in('user[name_last]', with: 'Shute')
-      fill_in('Email', with: 'barnany@barnany.com')
-      fill_in('user[job_title]', with: 'Freelance film maker')
-      # fill_in('#country', with: 'United Kingdom')
-      # fill_in('#town', with: 'London')
+      add_basic_details
       # fill_in('Website', with: "http://www.barnany.com")
       # fill_in('Linkedin', with: "http://linkedin.com/in/barnany")
-      fill_in('user[bio]', with: "Hi I'm Barney")
       click_button('Create my profile')
       expect(current_path).to eq '/users/5/show'
       expect(page).to have_content('Barnany Shute')
       expect(page).to have_content('Freelance film maker')
-      # expect(page).to have_content('London, United Kingdom')
       expect(page).to have_content('Hi I\'m Barney')
     end
 
-
-
+    it 'can add a location' do 
+      user_sign_up
+      add_basic_details
+      fill_in('#country', with: 'United Kingdom')
+      fill_in('#town', with: 'London')
+      expect(page).to have_content('London, United Kingdom')    
+    end 
   end 
 end
