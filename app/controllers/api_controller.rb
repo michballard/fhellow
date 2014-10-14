2f16e9e01a6e91d9af6d7d3f800c5dd9ad1ab2ac
Maya Driver <mayadrivermd@gmail.com>
2014-10-14 11:11:10 +0100

    added user info to map markers, updating the json api to give individual urls for avatars

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,8 +1,10 @@
 class ApiController < ApplicationController
 
   def getuser
-    @users = User.all
-    render json: @users.sort{|a,b| a.created_at <=> b.created_at}
+    @users = User.all.map do |user|
+         {full_name: "#{user.name_first} #{user.name_last}", longitude: user.longitude, latitude: user.latitude, image_url: user.avatar.url(:thumb), job_title: user.job_title, bio_truncated: (user.bio.slice(0, 50) + "...") }
+    end
+    render json: @users
   end
 
 end
