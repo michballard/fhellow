7b68c8d3cf326a9d4509aa2c6bc7173ba6419a50
Maya Driver <mayadrivermd@gmail.com>
2014-10-15 16:42:51 +0100

    Adding checkboxes for user interests to edit view and profile controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141015131641) do
+ActiveRecord::Schema.define(version: 20141015153145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,12 @@ ActiveRecord::Schema.define(version: 20141015131641) do
     t.string   "linkedin"
     t.string   "provider"
     t.string   "uid"
+    t.boolean  "discussing_ideas"
+    t.boolean  "afterwork_drinks"
+    t.boolean  "collaborating"
+    t.boolean  "chat_over_a_coffee"
+    t.boolean  "lunch_buddy"
+    t.boolean  "advising"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
