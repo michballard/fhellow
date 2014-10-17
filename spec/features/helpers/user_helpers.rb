30124e6393bfd3a3073cb39a2f306d284bb297b0
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-17 18:07:21 +0100

    added test for fhellows displaying and filtering on homepage

--- spec/features/helpers/user_helpers.rb
+++ spec/features/helpers/user_helpers.rb
@@ -9,16 +9,52 @@ module SessionHelpers
 	end
 
 	def add_basic_details
-    fill_in('user[name_first]', with: 'Barnany')
-    fill_in('user[name_last]', with: 'Shute')
-    fill_in('Email', with: 'barnany@barnany.com')
-    fill_in('user[job_title]', with: 'Freelance film maker')
-    fill_in('user[bio]', with: "Hi I'm Barney")
+	    fill_in('user[name_first]', with: 'Barnany')
+	    fill_in('user[name_last]', with: 'Shute')
+	    fill_in('Email', with: 'barnany@barnany.com')
+	    fill_in('user[job_title]', with: 'Freelance film maker')
+	    fill_in('user[bio]', with: "Hi I'm Barney")
 	end
 
   def add_bob_details
-      fill_in('user[name_last]', with: 'Shute')
-      fill_in('user[job_title]', with: 'Freelance film maker')
+	fill_in('user[name_last]', with: 'Shute')
+	fill_in('user[job_title]', with: 'Freelance film maker')
   end
+
+	def create_barnany
+	    User.create(email: "barnany@barnany.com",
+	                password: "123456789",
+					name_first: "Barnany",
+					name_last: "Shute",
+					job_title: "Freelance film maker",
+					town: "London",
+					bio: "Hi I'm Barney",
+					latitude: 51.522783,
+					longitude: -0.082867,
+					discussing_ideas: true, 
+					afterwork_drinks: true, 
+					collaborating: true, 
+					chat_over_a_coffee: true, 
+					lunch_buddy: true, 
+					advising: true)
+	end
+
+	def create_gemma
+		User.create(id: 10, email: "Gemma@gemma.com",
+							password: "123456789",
+							name_first: "Gemma",
+							name_last: "Gaven",
+							job_title: "Freelance PR",
+							location: "London",
+							bio: "",
+							latitude: 51.529363,
+							longitude: -0.085927,
+							discussing_ideas: false, 
+							afterwork_drinks: false, 
+							collaborating: false, 
+							chat_over_a_coffee: false, 
+							lunch_buddy: false, 
+							advising: false)
+	end
 
 end 
---
+++ spec/features/user_search_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require_relative 'helpers/user_helpers'
+
+include SessionHelpers
+
+describe 'Searching for fhellows on the homepage' do 
+
+	context 'Filtering by interests', js: true do 
+		before do
+			create_barnany
+			create_gemma
+		end
+
+		it 'displays all users when no filters are selected' do 
+			visit '/'
+			expect(page).to have_content 'Barnany'
+			expect(page).to have_content 'Gemma'
+		end
+
+		it "displays only those interested in having lunch buddies when it's selected" do 
+			visit '/'
+			check('lunch-buddy')
+			save_and_open_page
+			expect(page).to have_content 'Barnany'
+			expect(page).not_to have_content 'Gemma'
+		end
+	end
+
+
+
+end
