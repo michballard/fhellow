eb124dafbf917a586859c741d5fe56079f164441
ruthearle <rrearle@gmail.com>
2014-10-17 11:44:33 +0100

    Passed test for showing all posts on activity index page

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -1,15 +1,15 @@
 class ActivityController < ApplicationController
 
     def show
-         @user = User.find(params[:user_id])
-         @posts = Post.all
-         unless @user == current_user
-           flash[:notice] = 'Please sign in to see Fhellow activity'
-           redirect_to '/users/sign_in'
-         end
+      @user = User.find(params[:user_id])
+      @posts = Post.all
+      unless @user == current_user
+         flash[:notice] = 'Please sign in to see Fhellow activity'
+         redirect_to '/users/sign_in'
+      end
     end
 
     def index
-
+      @posts = Post.all
     end
 end
