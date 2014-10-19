695d7bfad8e9723514bddcc1a170caddb0b63065
Maya Driver <mayadrivermd@gmail.com>
2014-10-19 14:38:35 +0100

    refactored and passed first messaging test - sending a message

--- spec/features/message_feature_spec.rb
+++ spec/features/message_feature_spec.rb
@@ -5,29 +5,27 @@ include SessionHelpers
 
 describe 'sending messages' do 
 
-  def sign_in_user_one
-    user_sign_up
-    add_basic_details
-  end
-
-  def sign_in_user_two
-    second_user_sign_up
-    second_user_add_basic_details
-  end
-
   context 'a signed in user', js: true do 
 
+  before(:each) do
+    @bob = User.create(email: 'bob@b.com', password: '1234567890', password_confirmation: '1234567890')
+    @john = User.create(email: 'john@b.com', password: '1234567890', password_confirmation: '1234567890')
+      visit '/'
+      click_link 'Log in'
+      fill_in('Email', :with => 'john@b.com')
+      fill_in('Password', :with => '1234567890')
+      click_button 'Log in'
+  end
+
   	it 'can send a message' do 
-  		sign_in_user_one
-      visit '/message/1'
+      visit "/message/#{@bob.id}"
       fill_in('message', :with => 'Hello')
       find_button('Send').click
-      expect(current_path).to eq '/message/1'
+      expect(current_path).to eq "/message/#{@bob.id}"
       expect(page).to have_content('Hello')
   	end 
 
   	xit 'can receive a message' do 
-      sign_in_user_one
       visit '/message/2'
       fill_in('message', :with => 'Hello 2')
       find_button('Send').click
