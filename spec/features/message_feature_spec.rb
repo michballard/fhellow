d7e303fd4e6adcc5f2edebe2044139bd4a4283c8
ruthearle <rrearle@gmail.com>
2014-10-19 14:54:03 +0100

    second test passing for person receiving a message

--- spec/features/message_feature_spec.rb
+++ spec/features/message_feature_spec.rb
@@ -3,39 +3,52 @@ require_relative 'helpers/user_helpers'
 
 include SessionHelpers
 
-describe 'sending messages' do 
+describe 'sending messages' do
 
-  context 'a signed in user', js: true do 
+  def john_sign_in
+    visit '/'
+    click_link 'Log in'
+    fill_in('Email', :with => 'john@b.com')
+    fill_in('Password', :with => '1234567890')
+    click_button 'Log in'
+  end
+
+  def bob_sign_in
+    visit '/'
+    click_link 'Log in'
+    fill_in('Email', :with => 'bob@b.com')
+    fill_in('Password', :with => '1234567890')
+    click_button 'Log in'
+  end
+
+  context 'a signed in user', js: true do
 
   before(:each) do
     @bob = User.create(email: 'bob@b.com', password: '1234567890', password_confirmation: '1234567890')
     @john = User.create(email: 'john@b.com', password: '1234567890', password_confirmation: '1234567890')
-      visit '/'
-      click_link 'Log in'
-      fill_in('Email', :with => 'john@b.com')
-      fill_in('Password', :with => '1234567890')
-      click_button 'Log in'
   end
 
-  	it 'can send a message' do 
+  	it 'can send a message' do
+      john_sign_in
       visit "/message/#{@bob.id}"
       fill_in('message', :with => 'Hello')
       find_button('Send').click
       expect(current_path).to eq "/message/#{@bob.id}"
       expect(page).to have_content('Hello')
-  	end 
+  	end
 
-  	xit 'can receive a message' do 
-      visit '/message/2'
+  	it 'can receive a message' do
+      bob_sign_in
+      visit "/message/#{@john.id}"
       fill_in('message', :with => 'Hello 2')
       find_button('Send').click
       click_link 'Sign out'
-      sign_in_user_two
-      visit '/message/1'
+      john_sign_in
+      visit "/message/#{@bob.id}"
       expect(page).to have_content('Hello 2')
-  	end 
+  	end
 
-  	xit 'can view history of messages' do 
+  	xit 'can view history of messages' do
       sign_in_user_one
       visit '/message/2'
       fill_in('message', :with => 'Hello 2')
@@ -47,9 +60,9 @@ describe 'sending messages' do
       find_button('Send').click
       visit '/messages'
       expect(page).to have_content('Hello 2')
-      expect(page).to have_content('Hello 1!')    
-  	end 
+      expect(page).to have_content('Hello 1!')
+  	end
 
-  end 
+  end
 
-end 
+end
