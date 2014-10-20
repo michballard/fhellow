22b3434fd61e99989dd2f9b565eca2c510ad23a1
michballard <michballard@gmail.com>
2014-10-20 13:50:26 +0100

    Added Q library

---
+++ app/assets/javascripts/socializations.js
@@ -0,0 +1,12 @@
+// $(document).ready(function(){
+
+//   $('.follow-toggle').on('click', function(event){
+//   	console.log('Hi');
+//   	var link = $(this);
+//   	event.preventDefault();
+//   	$.post(this.href, function(response){
+//   		link.text(response.follow);
+//   	});
+//   });
+// });
+
