5f8ed2a2682bbc2799076184acee43bbbea6ab75
ruthearle <rrearle@gmail.com>
2014-10-20 10:19:09 +0100

    passed test for making the Fhellow logo a link to the homepage

---
+++ spec/features/navigation_feature_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+
+describe "navigation" do
+
+  context "displaying a nav bar" do
+
+    it "should have the fhellow logo" do
+      visit '/'
+      expect(page).to have_css '#logo'
+    end
+
+    it "the Fhellow logo links to the home page" do
+      bob = User.create(email: 'bob@bob.com', password: 'password', password_confirmation: 'password')
+      visit "/activity/#{bob.id}"
+      click_link 'Fhellow'
+      expect(current_path).to eq homepage_index_path
+    end
+
+    it "should have a link for the 'About' page" do
+      visit '/'
+      expect(page).to have_link 'About'
+    end
+
+    it "should have an activity nav link" do
+      visit '/'
+      expect(page).to have_link 'Activity'
+    end
+
+    it "should have a link for 'Post' page" do
+      visit '/'
+      expect(page).not_to have_link 'Post'
+      user_sign_up
+      visit '/'
+      expect(page).to have_link 'Post'
+    end
+
+    it "should have a link to join as a user" do
+      visit '/'
+      expect(page).to have_link 'Join'
+    end
+
+    it "should have a link for 'Log in' page" do
+      visit '/'
+      expect(page).to have_link 'Log in'
+    end
+  end
+end
