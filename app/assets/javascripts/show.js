f750f4bb945a7daab539c82fa0b675e2eb40ed7e
michballard <michballard@gmail.com>
2014-10-21 17:34:15 +0100

    Counters for followers completed

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -2,47 +2,14 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 
-
-
-// $(document).ready(function() {
-
-// var promises;
-
-//   url = "/api/users";
-
-// //Putting out the initial list of users
-//     $.get(url, function(users){
-//       promises = populateUsers(users);
-
-//       Q.all(promises).then(function() {
-//         $('a.follow-toggle').on('click', function(event){
-//           event.preventDefault();
-//           var link = $(this).closest('a')
-//           $.post(this.href, function(response){
-//              link.text(response.follow)
-//           });
-//         });
-//       });
-//     });
-
-// function populateUsers(users){
-//       return users.map(function(user){
-//         var deferred = Q.defer();
-//         var template = $ ('.follow-template').html();
-
-//         $('.follow-container').append(Mustache.render(template, user));
-//         deferred.resolve(true);
-
-//         return deferred.promise;
-//       });
-// }
-
-// });
-
 $('a.profile-follow-toggle').on('click', function(event){
   event.preventDefault();
-  var link = $(this)
+  var link = $(this);
+  var followerCount = $('.follower-count');
+  var followingCount = $('.following-count');
   $.post(this.href, function(response){
-         link.text(response.follow)
+         link.text(response.follow);
+         followerCount.text(response.new_follower_count);
+         followingCount.text(response.new_following_count);
   });
-})
+});
--- app/controllers/socializations_controller.rb
+++ app/controllers/socializations_controller.rb
@@ -1,15 +1,18 @@
 class SocializationsController < ApplicationController
-
-    # before_filter :load_socializable
+  include ActionView::Helpers::TextHelper
 
   def togglefollow
     @other_user = User.find(params[:user_id])
     @current_user = User.find(params[:current_user_id])
     @current_user.toggle_follow!(@other_user)
     if @current_user.follows?(@other_user) 
-      render json: { follow: "Following" }
+      render json: { follow: "Following", 
+							      	new_follower_count: "#{pluralize(@other_user.followers(User).count, "Follower")}", 
+							      	new_following_count: @other_user.followees(User).count }
     else
-      render json: {follow: "Follow"}
+      render json: {follow: "Follow", 
+							      	new_follower_count: "#{pluralize(@other_user.followers(User).count, "Follower")}", 
+							      	new_following_count: @other_user.followees(User).count }
     end
   end
 
