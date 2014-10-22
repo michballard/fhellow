c1268389013a24d425a7ec4d0a79fc75fc6fbb1c
michballard <michballard@gmail.com>
2014-10-22 10:03:11 +0100

    Added tests for creating likes.

--- spec/features/likes_feature_spec.rb
+++ spec/features/likes_feature_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 describe 'liking posts' do
 
+	before do 
+    @bob = User.create(email: 'bob@test.com', password: '1234567890', name_first: 'Bob')
+    @bob.posts.create(content: 'Here is my post')
+	end 
+
+	context 'a guest user' do 
+
+		it 'can not like a post' do 
+      visit '/'
+      click_link 'Activity'
+      expect(page).not_to have_link 'Like'
+		end 
+
+	end 
+
+	context 'a signed in user' do 
+    
+    xit 'can see a link to like a post' do 
+      sign_in(bob@test.com)
+      visit '/'
+      click_link 'Activity'
+      expect(page).to have_link 'Like'  
+    end 
+	end 
+
 end
