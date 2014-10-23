67e37986ace42e81e456dd09445fcc451442a78a
Jerome Pratt <jerome.pratt@outlook.com>
2014-10-23 22:54:02 +0100

    fixing silly bug

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -22,9 +22,9 @@ class ApiController < ApplicationController
               bio_truncated: (user.bio.slice(0, 50) + "..."),
               town: user.town,
               user_id: user.id,
-              first_name: user.name_first
-              # follow_status: current_or_guest_user.follow_status(user)
-              # current_user_id: current_or_guest_user.id
+              first_name: user.name_first,
+              follow_status: current_or_guest_user.follow_status(user),
+              current_user_id: current_or_guest_user.id
             }
 
          end
@@ -38,9 +38,9 @@ class ApiController < ApplicationController
             bio_truncated: user.bio, 
             town: user.town, 
             user_id: user.id, 
-            first_name: user.name_first 
-            # current_user_id: current_or_guest_user.id 
-            # follow_status: current_or_guest_user.follow_status(user) 
+            first_name: user.name_first,
+            current_user_id: current_or_guest_user.id,
+            follow_status: current_or_guest_user.follow_status(user) 
           }
         end
     end
