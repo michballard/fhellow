e790c48a699fc199c41c9e6b1480a2fc1ad4bd33
michballard <michballard@gmail.com>
2014-10-23 16:26:15 +0100

    Completed styling of posts with isotope

--- app/assets/javascripts/activity.js
+++ app/assets/javascripts/activity.js
@@ -1,7 +1,15 @@
-$(window).load(function(){
-  var $container = $('#post-activity-container');
+$(document).ready(function() {
+  var url = window.location.href;
+  if (url.endsWith('/show') === false) {
+    $('.child-container').addClass('activity-child-container').removeClass('profile-child-container');
+  }
+  var $container = $('#activity-post-container');
   $container.isotope({
-    itemSelector: '.post-child-container',
+    itemSelector: '.activity-child-container',
     layoutMode: 'masonry'
   });
 });
+
+String.prototype.endsWith = function(suffix) {
+  return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
