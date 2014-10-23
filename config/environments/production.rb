6d24e8ed11639184ef1cfd23cbe04c58724915b2
Stephen Lloyd <stephen@Stephens-MacBook-Pro-2.local>
2014-10-23 17:47:56 +0100

    changed assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  #assets for heroku
+  config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
