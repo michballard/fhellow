6eac29b47bada1448b5ad36b1ceb454066ea23e7
ruthearle <rrearle@gmail.com>
2014-10-24 15:07:06 +0100

    set up AWS storage and adjusted the position of the profile avatar

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,11 @@
 class Post < ActiveRecord::Base
 
     belongs_to :user
-    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+    has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+                      :storage => :s3,
+                      :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+       validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
     reverse_geocoded_by :latitude, :longitude do | obj, results |
         if geo = results.first
@@ -13,4 +16,7 @@ class Post < ActiveRecord::Base
 
     after_validation :reverse_geocode
 
+    def s3_credentials
+      {:bucket => "fhellow-photos", :access_key_id => Rails.application.secrets.aws_access_key_id, :secret_access_key => Rails.application.secrets.aws_secret_access_key }
+    end
 end
